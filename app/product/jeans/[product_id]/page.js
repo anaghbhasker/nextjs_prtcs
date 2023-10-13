0a57bd47a5c2dd8d608a58a9772585d8215ba674
anaghbhasker <anaghbhasker@gmail.com>
2023-10-13 15:29:28 +0530

    TEST

---
+++ app/product/jeans/[product_id]/page.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div className='flex gap-5'>
+        <div>Here is the image will be</div>
+        <div>
+            <h2>Jorden Jean</h2>
+            <p>jsjdckjsckjhiufjnjhvn</p>
+            <h2>Price : $100</h2>
+            <button>Add Cart</button>
+        </div>
+    </div>
+  )
+}
+
+export default page
